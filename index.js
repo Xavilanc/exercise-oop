5bdf7bb3fc5802ad862efcdf7f1402d9376b732e
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-05-30 14:47:10 +0200

    step8

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-ctx.beginPath();
-ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
-ctx.fillStyle = "red";
-ctx.fill();
+//ctx.beginPath();
+//ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false);
+//ctx.fillStyle = "red";
+//ctx.fill();
 
 class Entity {
     constructor(x, y, radius) {
@@ -51,15 +51,37 @@ class Enemy extends Projectile {
     }
 };
 
+class Particle extends Enemy{
+    constructor(x,y,radius,color,velocity){
+        super(x,y,radius,color,velocity);
+        this.alpha = 1;
+    }
 
-const projectile = new Projectile(50, 50, 30, "blue", {x: 3, y: 3});
-projectile.draw();
+    draw(){
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.restore();
+    }
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+        this.alpha -= 0.01;
+  }
+}
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 10, "blue");
 
+const player = new Player(canvas.width/2,canvas.height/2,10,"blue");
+const projectile = new Projectile(50,50,30,'blue',{x:3,y:3});
 const projectiles = [];
-
-const enemies = []
+const enemies = [];
+const particles = []
+player.draw();
+projectile.draw();
 
 window.addEventListener("click", (event) => {
     const angle = Math.atan2(
@@ -82,24 +104,99 @@ window.addEventListener("click", (event) => {
     projectile.draw();
 });
 
+let animationId;
 function animate() {
-    requestAnimationFrame(animate);
+    animationId = requestAnimationFrame(animate);
 
     ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     player.draw();
 
-    projectiles.forEach((projectile) => projectile.update());
+    particles.forEach((particle, index) => {
+        if(particle.alpha <= 0) {
+            particles.splice(index, 1);
+        } else {
+            particle.update();
+        }
+    });
 
-    enemies.forEach((enemy) => enemy.update());
+    projectiles.forEach((projectile,index) => {
+        if (
+            projectile.x - projectile.radius < 0 ||
+            projectile.x + projectile.radius > canvas.width ||
+            projectile.y - projectile.radius < 0 ||
+            projectile.y + projectile.radius > canvas.height
+          ) {
+            projectiles.splice(index, 1);
+          }
+        projectile.update();
+    });
+
+    enemies.forEach((enemy, enemyIndex) => {
+        projectiles.forEach((projectile, projectileIndex) => {
+            const distance = Math.hypot(
+                projectile.x - enemy.x, 
+                projectile.y - enemy.y
+                );
+            if(distance - projectile.radius - enemy.radius <= 0) {
+                for (let i = 0; i < 8; i++) {
+                    particles.push(
+                        new Particle(
+                            projectile.x, 
+                            projectile.y,
+                            Math.random() * (3 - 1) +1,
+                            enemy.color,
+                            {
+                                x: (Math.random() - 0.5) * 3,
+                                y: (Math.random() - 0.5) * 3,
+                            }
+                        )
+                    );
+                }
+
+                if(enemy.radius - 10 > 5) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10,
+                      });
+                    setTimeout(() => {
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                } else {
+                    setTimeout(() => {
+                        enemies.splice(enemyIndex, 1);
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0);
+                }
+            }
+        });
+    
+    const distPlayerEnemy = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+        if (distPlayerEnemy - enemy.radius - player.radius <= 0) {
+          cancelAnimationFrame(animationId);
+        }
+    enemy.update();
+});
 }
+
+/*enemies.forEach((enemy, enemyIndex) => {
+    projectiles.forEach((projectile, projectileIndex) => {
+      const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+        if (distance - projectile.radius - enemy.radius <= 0) {
+           enemies.splice(enemyIndex, 1);
+           projectiles.splice(projectileIndex, 1);
+        }
+      }) 
+    enemy.update();
+  });*/
+
+
+
 
 
 function spawnEnemies() {
     setInterval(() => {
     const radius = Math.random() * (30 - 4) + 4;
-
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
